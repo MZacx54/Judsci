b722c2576a57d5b7976d49fe03b3d1a262d14546
Meshach Zachariah <meshachzax@gmail.com>
2026-02-25 07:06:22 +0100

    Temporarily enable DEBUG to diagnose 500 error in production

--- backend/jdpc_bauchi_api/settings.py
+++ backend/jdpc_bauchi_api/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY', default='django-insecure-default-key-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 # Always allow railway subdomains and custom domain in prod
